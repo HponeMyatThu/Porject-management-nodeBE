2ed1ea2d65c9f9fe4fadf4823bc893eaaede52da
hmthu <hponemyatthu.ojt@d3-sg.com>
2025-01-06 23:20:38 +0630

    add token generate function

--- app.js
+++ app.js
@@ -4,19 +4,35 @@ import models from './models/index.js';
 import cors from 'cors';
 import { default as handleCRUD } from './utils/handleCRUD.js';
 import { Port, BaseRoute } from './utils/env.js';
+import { authenticateUser } from './utils/generateToken.js';
 
 const { sequelize, BusinessOwner } = models;
 const { json } = bp;
 
 const app = express();
 app.use(json());
-
 app.use(cors());
 
-app.use(`${BaseRoute}/businessOwner/:id?`, handleCRUD(BusinessOwner));
+app.use(`${BaseRoute}/businessOwner/CRUD/:id?`, handleCRUD(BusinessOwner));
+
+app.post(`${BaseRoute}/businessOwner/login`, async (req, res, next) => {
+  try {
+    const result = authenticateUser(BusinessOwner, req.body.email);
+    if (result.error) {
+      return res.status(result.status).json({ error: result.error });
+    }
+    return res.status(result.status).json({ token: result.token });
+  } catch (err) {
+    next(err);
+  }
+});
 
 sequelize.sync().then(() => {
-  app.listen(Port, () => {
-    console.log(`App listening on port ${Port}!`);
-  });
+  app
+    .listen(Port, () => {
+      console.log(`App listening on port ${Port}!`);
+    })
+    .on('error', (err) => {
+      console.error('Server failed to start:', err);
+    });
 });
